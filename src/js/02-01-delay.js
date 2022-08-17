d632a5800ed8ba8d3bb24bade0158cad02800a5d
woshinivek <woshinivek@yahoo.com>
2022-08-17 16:02:49 -0400

    changes

---
+++ src/js/02-01-delay.js
@@ -0,0 +1,32 @@
+import '../sass/index.scss';
+
+const resultRef = document.querySelector('.result');
+
+const delay = ms => {
+  return new Promise(resolve => {
+    const time = ms;
+
+    setTimeout(() => {
+      resolve(time);
+    }, time);
+  });
+};
+
+// const logger = time => console.log(`Resolved after ${time}ms`);
+
+const logger = time =>
+  resultRef.insertAdjacentHTML(
+    'beforeend',
+    `<li>
+      <p>Promise Resolved after: <b> ${time}ms </b></p>
+    </li>`
+  );
+
+// function logerResult() {
+//   return ;
+// }
+
+// Вызовы функции для проверки
+delay(2000).then(logger); // Resolved after 2000ms
+delay(1000).then(logger); // Resolved after 1000ms
+delay(1500).then(logger); // Resolved after 1500ms
